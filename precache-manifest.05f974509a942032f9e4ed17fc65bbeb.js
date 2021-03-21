1cc7dca37229f2ecd9b88426de6a3a91ee44b082
Philippe <philippe@five.team>
2021-03-21 15:20:45 +0100

    Updates

---
+++ precache-manifest.05f974509a942032f9e4ed17fc65bbeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14fe7da0ce32701554cdeabf8ce967c3",
+    "url": "/simple-storage/index.html"
+  },
+  {
+    "revision": "dc785f9f5510e4c21a02",
+    "url": "/simple-storage/static/css/main.90fd764b.chunk.css"
+  },
+  {
+    "revision": "ee9382b473e3af0e2485",
+    "url": "/simple-storage/static/js/2.9ce86722.chunk.js"
+  },
+  {
+    "revision": "dc785f9f5510e4c21a02",
+    "url": "/simple-storage/static/js/main.2a54781c.chunk.js"
+  },
+  {
+    "revision": "ab5d2fa59649cebb9df2",
+    "url": "/simple-storage/static/js/runtime-main.e8524f8d.js"
+  }
+]);
